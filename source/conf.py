285fbc8a54d811a566778d20431c0541fc9f7152
yueyinqiu <yueyinqiu@outlook.com>
2024-03-13 20:43:25 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,9 @@ language = 'zh_CN'
 
 html_theme = "sphinx_book_theme"
 html_theme_options = {
-    "extra_footer": "这里是 YiJingFramework 的文档。",
+    "extra_footer": "这里是 YiJingFramework 的文档。前往 "
+                    "<a href=\"https://yjfwk.yueyinqiu.top/\">https://yjfwk.yueyinqiu.top/</a> "
+                    "查看更多项目。"
 }
 # html_logo = "taiji.png"
 html_title = "YiJingFramework"
