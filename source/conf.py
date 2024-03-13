755c568992fa9561dbda1534feeaca8e9f12b735
yueyinqiu <yueyinqiu@outlook.com>
2024-03-13 20:38:41 +0800

    gw

--- source/conf.py
+++ source/conf.py
@@ -1,8 +1,8 @@
-extensions = ["nbsphinx"]
+extensions = ["nbsphinx", "sphinx_favicon"]
 
 templates_path = []
 exclude_patterns = []
-html_static_path = []
+html_static_path = ["wwwroot"]
 
 project = "YiJingFramework"
 language = 'zh_CN'
@@ -11,8 +11,13 @@ html_theme = "sphinx_book_theme"
 html_theme_options = {
     "extra_footer": "这里是 YiJingFramework 的文档。",
 }
-# html_logo = "taiji32.png"
+# html_logo = "taiji.png"
 html_title = "YiJingFramework"
+favicons = [
+    {
+        "href": "taiji.png" 
+    },
+]
 
 nbsphinx_codecell_lexer = "csharp"
 pygments_style = "vs"
